5c2daabb7e41817af6131b3608a123db7bf64fb7
BloodyRosa <123466864+BloodyRosa@users.noreply.github.com>
2025-02-14 22:08:48 +1100

    Initially create nextjs app (#10)
    * initially create nextjs app
    
    * fix some issues according to KieraDOG's suggestions
    
    * Update tailwind.config.ts
    
    Co-authored-by: Long Zhao <zlong@outlook.com>
    
    ---------
    
    Co-authored-by: Tuo  Yuan <43769362+t0ury@users.noreply.github.com>
    Co-authored-by: Long Zhao <zlong@outlook.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Meta Town",
+  description: "Meta Town is a virtual social space where you can interact with friends in real-time, chat, and join multiplayer video meetings. Jump in and start your metaverse experience now!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Meta-Town</h1>
+    </div>
+  );
+}
