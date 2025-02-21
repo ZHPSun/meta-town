b99ab50f4811f28c6dffd162d6f601649d2382be
Long Zhao <long.zhao@qantashotels.com>
2025-02-21 18:47:25 +1100

    style: adjust padding in Filter component for improved layout

--- src/app/_components/Filter/Filter.tsx
+++ src/app/_components/Filter/Filter.tsx
@@ -4,7 +4,7 @@ import CreatedSpaces from './_components/CreatedSpaces'
 import LastVisited from './_components/LastVisited'
 
 const Filter: FC = () => (
-  <div className="flex justify-end gap-4 px-6 py-6">
+  <div className="flex justify-end gap-4 px-6 pb-6 pt-12">
     <LastVisited />
     <CreatedSpaces />
     <Input prefix={{ name: 'search', label: 'Search' }} />
