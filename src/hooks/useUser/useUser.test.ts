30c3f0f67f8cf538034624bf2c70e29e4fe7634d
Long Zhao <zlong@outlook.com>
2025-03-07 09:53:02 +1100

    feat: update useUser hook to conditionally fetch user data based on session state (#185)
    * feat: update useUser hook to conditionally fetch user data based on session state
    
    * test: mock useSession in useUser tests to simulate session state

--- src/hooks/useUser/useUser.test.ts
+++ src/hooks/useUser/useUser.test.ts
@@ -1,17 +1,37 @@
 import { renderHook } from '@testing-library/react'
 import useSWR from 'swr'
+import useSession from '@/hooks/useSession'
 import useUser from './useUser'
 import getUser from './utils/getUser'
 
 vi.mock('swr')
 const useSWRMock = vi.mocked(useSWR)
 
+vi.mock('@/hooks/useSession')
+const useSessionMock = vi.mocked(useSession)
+
 describe('useUser', () => {
   afterEach(() => {
     vi.resetAllMocks()
   })
 
+  test('calls useSWR with null key if session is false', () => {
+    useSessionMock.mockReturnValue({
+      data: false,
+      isLoading: false,
+    } as unknown as ReturnType<typeof useSession>)
+
+    renderHook(() => useUser())
+
+    expect(useSWRMock).toHaveBeenCalledWith(null, getUser)
+  })
+
   test('calls useSWR with key and fetcher', () => {
+    useSessionMock.mockReturnValue({
+      data: true,
+      isLoading: false,
+    } as unknown as ReturnType<typeof useSession>)
+
     renderHook(() => useUser())
 
     expect(useSWRMock).toHaveBeenCalledWith('user', getUser)
@@ -23,6 +43,11 @@ describe('useUser', () => {
       displayName: 'John Doe',
       avatar: 'dog',
     }
+
+    useSessionMock.mockReturnValue({
+      data: true,
+      isLoading: false,
+    } as unknown as ReturnType<typeof useSession>)
 
     useSWRMock.mockReturnValue({
       data: USER_DATA,
--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,8 +1,13 @@
 import useSWR, { SWRResponse } from 'swr'
+import useSession from '@/hooks/useSession'
 import getUser from './utils/getUser'
 
 type User = Awaited<ReturnType<typeof getUser>>
 
-const useUser = (): SWRResponse<User> => useSWR('user', getUser)
+const useUser = (): SWRResponse<User> => {
+  const { data: session } = useSession()
+
+  return useSWR(session ? 'user' : null, getUser)
+}
 
 export default useUser
